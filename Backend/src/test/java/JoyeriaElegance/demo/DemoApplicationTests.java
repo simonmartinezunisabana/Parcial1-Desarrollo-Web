b1575d5924658ae1816efd90bd65ebd427b9f0d8
simonmartinezunisabana <simonmaga@unisabana.edu.co>
2025-11-24 21:26:22 -0500

    Reorganización de carpetas Frontend y Backend

---
+++ Backend/src/test/java/JoyeriaElegance/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package JoyeriaElegance.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
