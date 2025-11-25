31129730556ed3b265e3762ec702ec17590b732b
R-Alejandro-R-S <rafaelrisa@unisabana.edu.co>
2025-11-25 13:14:00 -0500

    Conexion con MySQL
    se añadio Admin, Usuario Controller y sus respectivos Repositories

---
+++ Backend/src/main/java/JoyeriaElegance/demo/Entities/Usuario.java
@@ -0,0 +1,72 @@
+package JoyeriaElegance.demo.Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtosjoyeriaapi-productos") //aqui ta el nombre de la tabla del MySQL jijijaja
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
